c0e6aae70bc7fc7ea67d3ad94bcf6e90bcce15b6
NathyNic <nathalynicole07@hotmail.com>
2023-05-26 16:38:58 -0500

    Terminado la volteada inferior

--- Project.py
+++ Project.py
@@ -31,38 +31,14 @@ print(mitad_ancho)
 parte1 = imagen[:mitad_alto, :ancho]
 parte2 = imagen[mitad_alto:, :ancho]
 
-# Rotación
-grados = 180  # Ángulo de rotación
+# Voltea la mitad inferior vertical y horizontalmente
+parte_inferior_volteada = cv2.flip(parte2, -1)
 
-# Calcular el centro de rotación
-centro_rotacion = (mitad_ancho, mitad_alto)
+# Combina las dos partes de la imagen
+imagen_final = np.concatenate((parte1, parte_inferior_volteada), axis=0)
 
-# Realizar rotación en la parte 3
-M = cv2.getRotationMatrix2D(centro_rotacion, grados, 1)
-parte2_rotacion = cv2.warpAffine(parte2, M, (mitad_ancho, mitad_alto))
-
-# Traslación
-#dx = 50  # Desplazamiento horizontal
-#dy = 50  # Desplazamiento vertical
-
-# Realizar traslación en la parte 1
-#parte1_traslacion = cv2.warpAffine(parte1, np.float32([[1, 0, dx], [0, 1, dy]]), (mitad_ancho, mitad_alto))
-
-# Supongamos que ya tienes las partes modificadas: parte1_traslacion, parte2, parte3_rotacion, parte4
-
-# Obtener las dimensiones de las partes modificadas
-alto_parte = parte2_rotacion.shape[0]
-ancho_parte = parte2_rotacion.shape[1]
-
-# Crear una imagen vacía del tamaño total
-imagen_resultante = np.zeros((alto_parte * 2, 1001, 3), dtype=np.uint8)
-
-# Copiar las partes modificadas en la imagen resultante
-imagen_resultante[:alto_parte, :ancho_parte] = parte1
-imagen_resultante[:alto_parte, ancho_parte:] = parte2_rotacion
-
-
-# Mostrar la imagen resultante
-cv2.imshow('Imagen resultante', imagen_resultante)
+# Muestra la imagen original y la imagen final
+cv2.imshow("Imagen Original", imagen)
+cv2.imshow("Imagen Final", imagen_final)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
