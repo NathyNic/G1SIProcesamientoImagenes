6d486b2d61b4807bc14b6d2ca8a6b75c1464a41d
NathyNic <nathalynicole07@hotmail.com>
2023-05-27 12:49:51 -0500

    Se borran comentarios que no se necesita.

--- Project.py
+++ Project.py
@@ -3,14 +3,7 @@ import numpy as np
 
 ##Utilizando el comando imread de opencv, leeremos la imagen 1.jpeg y la guardaremos en la variable img
 imagen = cv2.imread("imagen1.jpg")
-## Utilizando el comando imshow de opencv, mostraremos la imagen en una ventana llamada ventana1
-#cv2.imshow("ventana1", img)
-##Controla el tiempo de muestreo de la señal de entrada por teclado
-#cv2.waitKey()
-## destruye o cierra las ventanas creadas por opencv
-#cv2.destroyAllWindows()
-# ##Guardamos la imagen contenida en la variable img en el archivo imagenGuardada1.jpeg
-# cv2.imwrite("imagenGuardada1.jpeg", img)
+
 
 # Obtener las dimensiones de la imagen
 alto, ancho = imagen.shape[:2]
